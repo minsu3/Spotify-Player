49835d279dd93633c542ed1a6f725ff3c2970919
bluegoo192 <silversteinarthur+git@gmail.com>
2020-04-18 17:05:32 -0700

    stub spotify integration

---
+++ src/integrations/spotify.js
@@ -0,0 +1,12 @@
+// From spotify developer dashboard
+const clientId = 'c684509b8e09406c8b08e1c6e9110d94';
+const CLIENT_SECRET_ENV_VARIABLE_NAME = 'CHIHUAHUA_SPOTIFY_SECRET';
+const clientSecret = process.env[CLIENT_SECRET_ENV_VARIABLE_NAME];
+
+if (clientSecret == null) {
+  console.error(
+    `Spotify client secret was not found; please set it in your environment
+    variables to use Spotify integration.  Until then, all attempts to
+    communicate with Spotify will just log to the console :)`
+  );
+}
