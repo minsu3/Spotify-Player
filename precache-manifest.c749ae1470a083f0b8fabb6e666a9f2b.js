805bac7358b5d397a10e6e3425cce394502b9785
Min Su Kim <kim97minsu@gmail.com>
2020-04-29 22:14:27 -0700

    Updates

---
+++ precache-manifest.c749ae1470a083f0b8fabb6e666a9f2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2b5cb8f15e2be6a7fee5ab2d37f924",
+    "url": "/chihuahua/index.html"
+  },
+  {
+    "revision": "bbd102ca45af4c160969",
+    "url": "/chihuahua/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a5424935e6416c9925b6",
+    "url": "/chihuahua/static/css/main.fe6803f3.chunk.css"
+  },
+  {
+    "revision": "bbd102ca45af4c160969",
+    "url": "/chihuahua/static/js/2.4354e2b2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/chihuahua/static/js/2.4354e2b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5424935e6416c9925b6",
+    "url": "/chihuahua/static/js/main.8939ce43.chunk.js"
+  },
+  {
+    "revision": "2bdca6e49d2b6ee49cbe",
+    "url": "/chihuahua/static/js/runtime-main.650ac8d3.js"
+  }
+]);
